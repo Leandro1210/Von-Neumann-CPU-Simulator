0c7f79fc21044332096b9f37cc0c4635a2ed65a0
Rafael Nunes <rnbarbosa02@gmail.com>
2023-04-07 21:15:21 -0300

    Versão funcional do ciclo
    Instruções completas

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ unsigned char memoria[154]; //cada char 8 bits //zerar todo o vetor com zero// v
 unsigned int mbr;
 unsigned short int mar;
 unsigned short int ibr;
-unsigned char ir;
+unsigned char ir = 0;
 unsigned short int imm;
 unsigned short int pc = 0;
 unsigned char e, l, g;
@@ -25,14 +25,13 @@ int main()
   {
     memoria[i]=0x00;
   }
-  
+
   memoria[0] = 0x13;//Load A
   memoria[1] = 0x10;//Posição 16
   memoria[2] = 0x14;//Load B
   memoria[3] = 0x12;//Posição 18
   memoria[4] = 0x02;//Add
-  memoria[5] = 0x15;//Store A
-  memoria[6] = 0x0a;//Posição do store
+  memoria[5] = 0x0;//Hlt
   memoria[18] = 0x02;
   memoria[16] = 0x01;
   //mar = pc
@@ -64,15 +63,19 @@ int main()
       mar++;
       mbr = mbr <<8;
       mbr = mbr | memoria[mar];
+
       //Decodificar
       ibr = mbr & 0xffff; //Palavra da direita salva 
       mbr = mbr >> 16;
-      ir = mbr >> 11;
-      mar = mbr & 0x7ff;
+      ir = mbr >> 8; //8 funcina como 11, 5 como 8
+      mar = mbr & 0xff;
+      imm = mar;
       
+
     }else if (lr == 1){
-      ir = ibr >> 11;
-      mar = ibr & 0x7ff;
+      ir = ibr >> 8; 
+      mar = ibr & 0xff;
+      imm = mar;
     }
 
     //Executar
@@ -154,23 +157,46 @@ int main()
       a = !a;
     }else if( ir == 0x0c)
     {
-      //je
+      if (e == 1)
+      {
+        pc = mar;
+      }
       
     }else if( ir == 0x0d)
     {
+      if (e == 0)
+      {
+        pc = mar;
+      }
       //jne
 
     }else if( ir == 0x0e)
     {
+      if (l == 1)
+      {
+        pc = mar;
+      }
       //jl
     }else if( ir == 0x0f)
     {
+      if (e == 1 || l==1)
+      {
+        pc = mar;
+      }
       //jle
     }else if( ir == 0x10)
     {
+      if (g == 1)
+      {
+        pc = mar;
+      }
       //jg
     }else if( ir == 0x11)
     {
+      if (e == 1 || g==1)
+      {
+        pc = mar;
+      }
       //jge
     }else if( ir == 0x12)
     {
@@ -179,10 +205,12 @@ int main()
     }else if( ir == 0x13)
     {
       //lda
+      
       a = memoria[mar];
     }else if( ir == 0x14)
     {
       //ldb
+      
       b = memoria[mar];
     }else if( ir == 0x15)
     {
@@ -206,36 +234,53 @@ int main()
       //moviah imm
     }else if( ir == 0x1a)
     {
+      a = a + imm;
       //addia imm
     }else if( ir == 0x1b)
-    {
+    {  
+      a = a - imm;
       //subia imm
     }else if( ir == 0x1c)
     {
+      a = a * imm;
       //mulia imm
     }else if( ir == 0x1d)
-    {
+    { 
+      a = a/ imm;
       //divia imm
     }else if( ir == 0x1e)
-    {
+    { 
+      a = a << imm;
       //lsh imm
     }else if( ir == 0x1f)
     {
+      a = a >> imm; 
       //rsh imm
     }
 
-    printf("CPU: \nA: 0x%x\t\tB: 0x%x\t\tT: 0x%x\t\t\nMBR: 0x%x\tIBR: 0x%x\tMAR: 0x%x\tIMM: 0x%x\t\t\nPC: 0x%x\t\tIR: 0x%x\t\tLR: 0x%x\t\t\nE: 0x%x\t\tL: 0x%x\t\tG: 0x%x\n\n", a, b, t, mbr, ibr, mar, imm, pc, ir, lr, e, l, g);
-    printf("Memoria:\n");
-
-    for (int i = 0; i < 0x99; i++)
+    if (lr == 0)
     {
-      printf("%d:0x%x ",i, memoria[i]);  
+      lr = 1;
+    }  
+    
+    else if (lr ==1)
+    {
+
+        printf("CPU: \nA: 0x%x\t\tB: 0x%x\t\tT: 0x%x\t\t\nMBR: 0x%x\tIBR: 0x%x\tMAR: 0x%x\tIMM: 0x%x\t\t\nPC: 0x%x\t\tIR: 0x%x\t\tLR: 0x%x\t\t\nE: 0x%x\t\tL: 0x%x\t\tG: 0x%x\n\n", a, b, t, mbr, ibr, mar, imm, pc, ir, lr, e, l, g);
+        printf("Memoria:\n");
+
+        for (int i = 0; i < 0x99; i++)
+        {
+          printf("%d:0x%x ",i, memoria[i]);  
+        }
+        printf("\n");
+        printf("\n\nAperte uma tecla para iniciar o proximo ciclo, ou Ctrl+C para finalizar\n");
+        getch();
+        i++;
+        lr = 0;  
+        pc =+4;
     }
-    printf("\n");
-    printf("\n\nAperte uma tecla para iniciar o proximo ciclo, ou Ctrl+C para finalizar\n");
-    getch();
-    i++;
-    pc +=4;
+
 
     } while (mar != 999);
   return 0;
